c3fe5591aa19b5ece003af31334c0758f623b55c
Whis <58374931+itzwhis@users.noreply.github.com>
2025-06-20 12:02:18 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
+
+name 'nx-headshot'
+description 'headshot detection cheater with screenshot'
+author 'Whis'
+version '1.1.0'
+
+
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
@@ -9,3 +16,6 @@ dependencies {
     'qb-core',
     'screenshot-basic'
 }
+
+--Discord--
+--https://discord.gg/tpAQDjuN9H
